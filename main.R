4aea1f09ea718daa9efb1d7367a545824b44748a
Anne-Mette Hartved Dahl <amhd@kb.dk>
2025-06-30 08:38:46 +0200

    Tilføjet få eksempler på brug af funktioner

--- main.R
+++ main.R
@@ -13,11 +13,15 @@ r_scripts <- list.files("R", pattern = "\\.R$", full.names = TRUE)
 invisible(lapply(r_scripts, source))
 
 
-overblik <- overblik_samlinger() # Overblik over samlingerne
-
-
 
 # --- Brug af funktionerne ---
 
+## overblik <- overblik_samlinger() # Overblik over samlingerne
+
+## alle_moeder <- hent_og_parse_moeder(year = c(2009:2024)) - henter alle møder
+
+## 20181_bertel <- hent_og_parse_moeder(samling_id = "20181", full_name = "Bertel Haarder) - henter alle møder fra samling 20181 hvor Bertel Haarder snakker
+
+## hent_opdateret_moedefiler(Sys.Date() - 7) - Henter de samlinger som er blevet opdateret den sidste uge 
 
 
